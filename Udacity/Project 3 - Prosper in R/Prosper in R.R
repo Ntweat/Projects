7c670d52322d5ec741354e71760b4a9eb8672591
ntweat <ntweat@ntweat.com>
2017-10-05 21:40:24 -0400

    NN week 3

---
+++ Udacity/Project 3 - Prosper in R/Prosper in R.R
@@ -0,0 +1,18 @@
+---
+Examine and Summarize Data Using R - Prosper
+=============================================
+**By: Saurabh Hinduja**
+  
+---
+  
+## Introduction
+This section contains information about the dataset selected, reasons why the dataset was selected 
+
+*Dataset Selected:* Propser
+
+**What is Propser**
+
+There are many peer to peer networks, like file sharing, meeting new friends and many more. Prosper is America's first and leading peer-to-peer lending network. In this kind of network, lenders research borrowers and lend them money directly. 
+
+Prosper works because it has lower rate of interest than banks and multiple lenders can contribute to the same borrower. Therefore, it is lucrative for borrowers because it has lower rate of interest and lenders like it cause it reduces their risk and can contribute towards a bigger loan amount. 
+
